aaa23d89c4a813fd85dc0458b7143571aaaee978
muhammadsaad1756 <chsaadali3000@gmail.com>
2024-04-07 02:27:05 +0100

    initial commit

---
+++ src/components/Todo.js
@@ -0,0 +1,30 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import { faPenToSquare, faTrash } from "@fortawesome/free-solid-svg-icons";
+
+export const Todo = ({ task, toggleComplete, deleteTodo, editTodo }) => {
+  return (
+    <div className="Todo">
+      <p
+        onClick={() => toggleComplete(task.id)}
+        className={`${task.completed ? "completed" : "incompleted"}`}
+      >
+        {task.task}
+      </p>
+      <div>
+        <FontAwesomeIcon
+          className="edit-icon"
+          icon={faPenToSquare}
+          onClick={() => editTodo(task.id)}
+        />
+        <FontAwesomeIcon
+          className="delete-icon"
+          icon={faTrash}
+          onClick={() => deleteTodo(task.id)}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Todo;
